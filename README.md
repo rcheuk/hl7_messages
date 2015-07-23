fd544d17c1ff814c570d35761dc928e011f496a0
Rachel Cheuk <rachel.cheuk@gmail.com>
2015-07-23 02:16:43 -0400

    updating results

---
+++ README.md
@@ -0,0 +1,6 @@
+# hl7 messages
+
+This application requires `node` and `npm`. To install the dependencies, run `npm install && bower install`.  From the root directory, run `grunt serve` to start the application.
+
+This application was scaffolded using Yeoman, with the angular-fullstack generator. It incorporates AngularJS, an express server for
+backend requests, and Bootstrap/Bootstrap UI for responsive design and UI/UX.
--- client/components/results/results.directive.js
+++ client/components/results/results.directive.js
@@ -3,7 +3,7 @@
 
   angular
     .module('hl7MessagesApp')
-    .directive('results', navbar);
+    .directive('result', navbar);
 
   /** @ngInject */
   function navbar() {
@@ -11,6 +11,11 @@
       restrict: 'E',
       templateUrl: 'components/results/results.html',
       controller: ResultsController,
+      scope: {
+        result: '=data',
+        resultType: '=type',
+        searchTerm: '=searchText'
+      },
       controllerAs: 'ctrl',
       bindToController: true
     };
@@ -20,6 +25,20 @@
     /** @ngInject */
     function ResultsController($location) {
       var ctrl = this;
+      console.log('result', ctrl.result, ctrl.resultType, ctrl.searchTerm);
+      ctrl.processResults = function() {
+        if (ctrl.result) {
+          var resultObj = {};
+          var segments = ctrl.result.split("\n");
+          console.log('segments', segments);
+          segments.map(function(s) {
+            console.log('s', s);
+            s.split("\|");
+            resultObj.segment = s[0];
+
+          });
+        }
+      }
 
     }
   }
